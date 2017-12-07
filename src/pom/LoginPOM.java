d7825d771f878d6b752050e7b03e747a3a487a34
Megha189 <32758865+Megha189@users.noreply.github.com>
2017-12-07 22:06:19 +0530

    LoginPOM class error checking is added

--- src/pom/LoginPOM.java
+++ src/pom/LoginPOM.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.Reporter;
 
 public class LoginPOM {
 	
@@ -13,6 +16,8 @@ public class LoginPOM {
 	private WebElement pwTB;
 	@FindBy(xpath="//button[.='Login']")
 	private WebElement loginBTN; 
+	@FindBy(xpath="//div[contains(text(),'information')]")
+	private WebElement errMSG;
 	public LoginPOM(WebDriver driver)
 	{
 		PageFactory.initElements(driver,this);
@@ -28,6 +33,20 @@ public class LoginPOM {
 	public void clickLogin()
 	{
 		loginBTN.click();
+	}
+	public void verifyerrormsg(WebDriver driver)
+	{
+		String errText=errMSG.getText();
+		Reporter.log("errtext:"+errText);
+		if(errMSG.isDisplayed())
+		{
+			Reporter.log("pass:error is dipalyed..",true);
+		}
+		else
+		{
+			Reporter.log("Fail:Error message is not dispalyed..",true);
+			Assert.fail();
+		}
 	}
 
 }
